5055fbe2e48eb76449d2e716dd4a92c1cfe1aea9
D-Waack <danielmwaack@gmail.com>
2023-05-25 17:50:05 -0300

    Add plano de fundo na interface

--- Projeto/World/Maps/Common/Platforms.gd
+++ Projeto/World/Maps/Common/Platforms.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
 # Variável de controle
-var has_grabbed # objeto foi selecionado
+var has_grabbed = false # objeto foi selecionado
 
 # Sinal para update de posição
 signal fit_to_grid
+signal kill_player
 
 # Função roda na inicialização do nó. Conectando sinal todos os objetos filhos automaticamente
 func _ready():
 	for object in get_children():
 		object.connect("fit_to_grid", _on_Object_fit_to_grid)
+		object.connect("kill_player", _on_Object_kill_player)
 
 # Função roda a todo frame
 func _process(_delta):
@@ -30,3 +32,6 @@ func block_grabbing(condition):
 # Emite sinal acima de fit_to_grid dos objetos acima para o mapa
 func _on_Object_fit_to_grid(object):
 	emit_signal("fit_to_grid", object)
+
+func _on_Object_kill_player():
+	emit_signal("kill_player")
