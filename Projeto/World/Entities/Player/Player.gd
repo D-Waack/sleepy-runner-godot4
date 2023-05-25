ed1917f94194d9afeb62cecc7939aa4de995f12d
D-Waack <danielmwaack@gmail.com>
2023-05-24 22:14:31 -0300

    Update do script do Player, adicao de comentarios e reorganizacao

--- Projeto/World/Entities/Player/Player.gd
+++ Projeto/World/Entities/Player/Player.gd
@@ -1,19 +1,25 @@
 extends CharacterBody2D
 
+# Declaração de classe
 class_name Player
 
+# Constantes e variáveis físicas
 const SPEED = 210.0
 const JUMP_VELOCITY = -400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+# Variáveis de controle
 var facing_direction = 1
 
+# Sinais
 signal dead
 
+# Desativa o processo físico no início da execução
 func _ready():
 	set_physics_process(false)
 
+# Roda a todo frame. Verificações e cálculos de física
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -29,10 +35,12 @@ func _physics_process(delta):
 
 	move_and_slide()
 
+# "Mata" o personagem quando necessário
 func kill():
 	set_physics_process(false)
 	emit_signal("dead")
 
+# Reinicia o processo físico e reseta o personagem à posição padrão
 func reset_start():
 	set_physics_process(true)
 	facing_direction = 1
