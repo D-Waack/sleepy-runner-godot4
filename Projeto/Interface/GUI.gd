fcc362fdf02f23a5607b1b431c897aecc46cc403
D-Waack <danielmwaack@gmail.com>
2023-05-25 19:46:58 -0300

    Update do GUI, background, painel de vitoria

--- Projeto/Interface/GUI.gd
+++ Projeto/Interface/GUI.gd
@@ -3,6 +3,11 @@ extends Control
 # Variáveis de nós
 @onready var play_button = $CanvasLayer/PlayButton
 @onready var victory_panel = $CanvasLayer/VictoryPanel
+@onready var shine_display = $CanvasLayer/VictoryPanel/ShineDisplay
+@onready var puzzle_display = $CanvasLayer/VictoryPanel/PuzzleDisplay
+
+# constantes
+const tile_size = 16
 
 # Sinal para o botão de play
 signal play_button_pressed
@@ -17,5 +22,13 @@ func soft_reset():
 	play_button.set_visible(true)
 
 # Mostra o painel de vitória provisório
-func show_victory():
+func show_victory(shine_count, puzzle_get):
 	victory_panel.set_visible(true)
+	if shine_count > 0:
+		shine_display.set_size(Vector2(shine_count * tile_size, tile_size))
+	else:
+		shine_display.set_visible(false)
+	if puzzle_get:
+		puzzle_display.set_size(Vector2(tile_size, tile_size))
+	else:
+		puzzle_display.set_visible(false)
