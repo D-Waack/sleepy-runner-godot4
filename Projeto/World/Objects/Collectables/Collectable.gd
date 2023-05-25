dc8791048cd1d49492ddc137ece8b8add9e484c7
D-Waack <danielmwaack@gmail.com>
2023-05-25 19:49:46 -0300

    Updates de texturas para o painel de vitoria

--- Projeto/World/Objects/Collectables/Collectable.gd
+++ Projeto/World/Objects/Collectables/Collectable.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
+@export var type = 0
+
 signal collected
 
 func _on_body_entered(body):
 	if body is Player:
-		emit_signal("collected")
+		emit_signal("collected", type)
 		queue_free()
