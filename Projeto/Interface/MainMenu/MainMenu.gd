e50472dedcec6606d1977c8702de922d1d9ee330
D-Waack <danielmwaack@gmail.com>
2023-05-29 17:49:56 -0300

    Add texturas e menu principal

---
+++ Projeto/Interface/MainMenu/MainMenu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+@onready var player = $Player
+@onready var animator = $AnimationPlayer
+
+func _ready():
+	animator.play("run")
+	player.set_physics_process(true)
+	player.SPEED = 120.0
+
+func player_jump():
+	player.jumped = true
+	player.animator.travel("rise")
+
+func adjust_player():
+	player.facing_direction = -1
+	player.position = Vector2(350, 66)
+
+func adjust_player2():
+	player.facing_direction = 1
+	player.position = Vector2(-15, 178)
+
+func _on_start_button_button_up():
+	get_tree().change_scene_to_file("res://World/Maps/StageSelect.tscn")
+
+func _on_quit_button_button_up():
+	get_tree().quit()
---
+++ Projeto/Interface/Textures/GUI.gd
@@ -0,0 +1,71 @@
+extends Control
+
+# Variáveis de nós
+@onready var level_edit = $CanvasLayer/LevelEdit
+@onready var run = $CanvasLayer/Run
+@onready var pause = $CanvasLayer/Pause
+@onready var victory_panel = $CanvasLayer/VictoryPanel
+@onready var shine_display = $CanvasLayer/VictoryPanel/ShineDisplay
+@onready var puzzle_display = $CanvasLayer/VictoryPanel/PuzzleDisplay
+
+# constantes
+const tile_size = 16
+
+# Sinal para o botão de play
+signal play_button_pressed
+signal restart_button_pressed
+signal pause_button_pressed
+signal jump_button_pressed
+signal resume_button_pressed
+signal edit_button_pressed
+signal exit_button_pressed
+
+# Emite o sinal para o botão de play
+func _on_play_button_button_up():
+	emit_signal("play_button_pressed")
+
+func _on_restart_button_button_up():
+	emit_signal("restart_button_pressed")
+
+func _on_pause_button_button_up():
+	emit_signal("pause_button_pressed")
+
+func _on_jump_button_button_up():
+	emit_signal("jump_button_pressed")
+
+func _on_resume_button_button_up():
+	emit_signal("resume_button_pressed")
+
+func _on_edit_button_button_up():
+	emit_signal("edit_button_pressed")
+
+func _on_exit_button_button_up():
+	emit_signal("exit_button_pressed")
+
+# Retorna a visibilidade dos botões de edição
+func set_level_edit():
+	level_edit.set_visible(true)
+	pause.set_visible(false)
+	run.set_visible(false)
+
+func set_run():
+	level_edit.set_visible(false)
+	run.set_visible(true)
+	pause.set_visible(false)
+
+func set_pause():
+	level_edit.set_visible(false)
+	run.set_visible(false)
+	pause.set_visible(true)
+
+# Mostra o painel de vitória provisório
+func show_victory(shine_count, puzzle_get):
+	victory_panel.set_visible(true)
+	if shine_count > 0:
+		shine_display.set_size(Vector2(shine_count * tile_size, tile_size))
+	else:
+		shine_display.set_visible(false)
+	if puzzle_get:
+		puzzle_display.set_size(Vector2(tile_size, tile_size))
+	else:
+		puzzle_display.set_visible(false)
