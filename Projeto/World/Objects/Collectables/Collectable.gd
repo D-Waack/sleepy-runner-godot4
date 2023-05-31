1698d0657677434b82c08f2571aa0e3aecf66aa7
D-Waack <danielmwaack@gmail.com>
2023-05-31 18:54:08 -0300

    Ajuste de colisoes para inclusao de flecha

--- Projeto/World/Objects/Collectables/Collectable.gd
+++ Projeto/World/Objects/Collectables/Collectable.gd
@@ -2,9 +2,13 @@ extends Area2D
 
 @export var type = 0
 
+var been_collected
+
 signal collected
 
 func _on_body_entered(body):
-	if body is Player:
+	if been_collected: return
+	if body is Player or body is Arrow:
+		been_collected = true
 		emit_signal("collected", type)
 		queue_free()
