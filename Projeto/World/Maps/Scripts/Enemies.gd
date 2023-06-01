68731fae50bdd69f282b525f7bf4d53e16b53da6
D-Waack <danielmwaack@gmail.com>
2023-06-01 05:39:32 -0300

    Add script para gerenciamento de inimigos no mapa

---
+++ Projeto/World/Maps/Scripts/Enemies.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+signal kill_player
+
+func _ready():
+	for enemy in get_children():
+		enemy.connect("kill_player", _on_Enemy_kill_player)
+
+func _on_Enemy_kill_player():
+	emit_signal("kill_player")
