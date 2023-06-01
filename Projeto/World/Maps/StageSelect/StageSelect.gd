c627fca0997b6fc0b91ee224258ab9b2a8d94efb
D-Waack <danielmwaack@gmail.com>
2023-06-01 07:11:49 -0300

    Add botao de retornar ao menu principal na tela de stage select

--- Projeto/World/Maps/StageSelect/StageSelect.gd
+++ Projeto/World/Maps/StageSelect/StageSelect.gd
@@ -6,3 +6,6 @@ func _ready():
 	var stats = SaveManager.stats
 	for stage in stages.get_children():
 		stage.set_stat(stats.world1[stage.stage_index])
+
+func _on_return_button_button_up():
+	get_tree().change_scene_to_file("res://Interface/MainMenu/MainMenu.tscn")
