1bdfae844d4af2e2473afbba045fe8c169aae518
D-Waack <danielmwaack@gmail.com>
2023-06-03 14:12:34 -0300

    Add sons novos, add novo formato para bgm

--- Projeto/Interface/MainMenu/MainMenu.gd
+++ Projeto/Interface/MainMenu/MainMenu.gd
@@ -2,11 +2,12 @@ extends Control
 
 @onready var player = $Player
 @onready var animator = $AnimationPlayer
-@onready var audio = $AudioStreamPlayer
+@onready var bgm = $BGM
 
 func _ready():
+	
 	SaveManager.prepare_save()
-	audio.play()
+	bgm.play()
 	animator.play("run")
 	player.set_physics_process(true)
 	player.SPEED = 120.0
@@ -24,12 +25,14 @@ func adjust_player2():
 	player.position = Vector2(-15, 178)
 
 func _on_start_button_button_up():
+	SoundManager.play_option()
 	get_tree().change_scene_to_file("res://World/Maps/StageSelect/StageSelect.tscn")
 
 func _on_quit_button_button_up():
 	get_tree().quit()
 
 func _on_delete_save_button_button_up():
+	SoundManager.play_option()
 	if not SaveManager.save_exists(): return
 	SaveManager.delete_savegame()
 	get_tree().reload_current_scene()
