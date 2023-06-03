09daa385d3789565c1e0e68a0f79ecb2c5698117
D-Waack <danielmwaack@gmail.com>
2023-06-03 16:16:35 -0300

    Add funcoes para novos efeitos sonoros

--- Projeto/Managing/SoundManager.gd
+++ Projeto/Managing/SoundManager.gd
@@ -2,6 +2,14 @@ extends Node
 
 # Sons globais
 @onready var click_sound = $ClickSound
+@onready var enemy_death_sound = $EnemyDeathSound
+@onready var crystal_break_sound = $CrystalBreakSound
 
 func play_option():
 	click_sound.play()
+
+func play_enemy_death():
+	enemy_death_sound.play()
+
+func play_crystal_break():
+	crystal_break_sound.play()
