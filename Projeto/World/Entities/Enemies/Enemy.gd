9cd2f1f19ad9e785a0ce24bba673edd5235d6d85
D-Waack <danielmwaack@gmail.com>
2023-06-03 16:17:01 -0300

    Add classe pai para inimigos

---
+++ Projeto/World/Entities/Enemies/Enemy.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+class_name Enemy
+
+@onready var tween_values = [0, 6]
+@onready var sprite = $AnimatedSprite2D
+
+var tween 
+
+signal kill_player
+
+func _ready():
+	tween_values[0] = position.y
+	tween_values[1] += position.y
+	sprite.play("default")
+	start_tween()
+
+func start_tween():
+	tween = create_tween()
+	tween.tween_property(self, "position:y", tween_values[1], 0.3)
+	await tween.finished
+	tween.kill()
+	tween_values.reverse()
+	start_tween()
+	#tween_property(object: Object, property: NodePath, final_val: Variant, duration: float)
+
+func _on_hitbox_body_entered(body):
+	if body is Player:
+		emit_signal("kill_player")
+
+func kill():
+	SoundManager.play_enemy_death()
+	queue_free()
